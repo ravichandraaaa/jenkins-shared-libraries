cfadc814188676f9d2833a528cfe7b7649ef84d2
ravichandraaaa <ravichandrakotapati68@gmail.com>
2026-02-13 17:46:14 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,19 +1,24 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'docker-hub-credentials'
-    
-    echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
+def call(Map config) {
+
+    def imageName = config.imageName
+    def imageTag = config.imageTag
+    def credentials = config.credentials
+
     withCredentials([usernamePassword(
         credentialsId: credentials,
-        usernameVariable: 'DOCKER_USERNAME',
-        passwordVariable: 'DOCKER_PASSWORD'
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
     )]) {
+
         sh """
-            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            
             docker push ${imageName}:${imageTag}
             docker push ${imageName}:latest
+            
+            docker logout
         """
     }
+
+    echo "Docker image pushed: ${imageName}:${imageTag}"
 }
